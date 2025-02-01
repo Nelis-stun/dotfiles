73412741f9421115506c79383c2c8037b04a3c61
heavydemon21 <nielsstunnebrink1@gmail.com>
2025-02-01 19:10:48 +0100

    updated neovim was not latest

--- install.sh
+++ install.sh
@@ -6,17 +6,21 @@ echo "Setting up dotfiles"
 sudo apt update
 sudo apt upgrade
 
-sudo apt install -y curl kitty zsh i3 polybar git fonts-firacode cmake make build-essential gcc zip neovim npm ripgrep g++ libfuse2
+sudo apt install -y curl kitty zsh i3 polybar git fonts-firacode cmake make build-essential gcc zip neovim npm ripgrep g++ libfuse2 python3-venv python3-pip picom rofi
 
 # installing neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+curl -LO https://github.com/neovim/neovim/releases/download/0.10.4/nvim-linux-x86_64.appimage
 sudo chmod +x nvim-linux-x86_64.appimage
 sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
 
 # installing rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+#install ohmyzsh
 curl -L https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+
+#install browser
+curl -fsS https://dl.brave.com/install.sh | sh
 
 if [ -f "$HOME/.zshrc" ]; then
     echo "Removing existing .zshrc..."
