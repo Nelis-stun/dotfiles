244a0bdbc777d33f4aa6c56023ff194a39392825
heavydemon21 <nielsstunnebrink1@gmail.com>
2025-02-01 12:51:40 +0100

    install script ubuntu

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Setting up dotfiles"
+
+# Install necessary packages
+sudo apt update
+
+sudo apt install -y curl kitty zsh i3 polybar git fonts-firacode cmake make build-essential gcc
+
+# installing neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
+# installing rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+sh ./symlinker.sh
+
+# Set Zsh as default shell
+chsh -s $(which zsh)
+
+echo "Dotfiles setup complete! Please restart your terminal."
---
+++ symlinker.sh
@@ -0,0 +1,6 @@
+# Create symlinks
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.config/kitty ~/.config/kitty
+ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+ln -s ~/.dotfiles/.config/polybar ~/.config/polybar
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
